540a4934d3361db1fa8d292ed6cf548f9d277ff8
Alan Pacheco <alan.estudo@gmail.com>
2022-01-16 02:44:01 -0300

    Save, useNavigate

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -10,7 +10,7 @@ function Navbar() {
                     <a href="https://github.com/alanpacheco">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className="dsmovie-contact-link">/devsuperior</p>
+                            <p className="dsmovie-contact-link">/alanpacheco</p>
                         </div>
                     </a>
                 </div>
